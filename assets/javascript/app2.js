cea4505dead8809d428694e4daf5991767ff3ac4
Hugo <52917408+hugo-engineer@users.noreply.github.com>
2019-09-14 09:55:14 +1000

    Separated the Google Place JS into app2.js

---
+++ assets/javascript/app2.js
@@ -0,0 +1,21 @@
+$("#search").on("change", function (e) {
+    e.preventDefault();
+    var kw = $("#search").val();
+    console.log(kw);
+    
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyATo66aR1XW_0vPRGB6CjsrCBDjaYi9ZUM&input=" + kw + "&inputtype=textquery&fields=opening_hours,icon,geometry,formatted_address,geometry,icon,name,permanently_closed,photos,place_id,plus_code,types";
+    
+    $.ajax({
+        url: queryURL,
+        method: 'get',
+        // headers: {
+        //     'Access-Control-Allow-Origin': '*'
+        // },
+        cache: false,
+    }).then(function (list) {
+
+        console.log(list)
+
+    });
+
+});
