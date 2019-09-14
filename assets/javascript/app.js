d541a8aea7d9e1daacb2a633e68d1c890f5583b7
Hugo <52917408+hugo-engineer@users.noreply.github.com>
2019-09-14 11:26:55 +1000

    updated google address search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,16 @@
 
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyAO_Gz79glGQcOnEg5cs4vi4sN3VQ5dYvM",
-    authDomain: "lunchmocracy.firebaseapp.com",
-    databaseURL: "https://lunchmocracy.firebaseio.com",
-    projectId: "lunchmocracy",
-    storageBucket: "",
-    messagingSenderId: "264715414255",
-    appId: "1:264715414255:web:6a32965e17f1d93f47491a"
-  };
+//   // Your web app's Firebase configuration
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyAO_Gz79glGQcOnEg5cs4vi4sN3VQ5dYvM",
+//     authDomain: "lunchmocracy.firebaseapp.com",
+//     databaseURL: "https://lunchmocracy.firebaseio.com",
+//     projectId: "lunchmocracy",
+//     storageBucket: "",
+//     messagingSenderId: "264715414255",
+//     appId: "1:264715414255:web:6a32965e17f1d93f47491a"
+//   };
   
-  // Initialize Firebase
+//   // Initialize Firebase
 
-  firebase.initializeApp(firebaseConfig);
+//   firebase.initializeApp(firebaseConfig);
