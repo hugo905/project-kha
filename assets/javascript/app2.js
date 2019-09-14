ae2152ad6233b39e8cfa6ed747c6e321380d95b5
Hugo <52917408+hugo-engineer@users.noreply.github.com>
2019-09-14 13:19:33 +1000

    Updated google api with place ID + image search

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,27 +1,43 @@
 $("#submit").on("click", function (e) {
     e.preventDefault();
     var kw = $("#search").val();
-    console.log(kw);
-    
-var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + kw + "&inputtype=textquery&fields=photos,formatted_address,geometry,icon,name,permanently_closed,price_level,user_ratings_total,opening_hours,rating&locationbias=circle:2000@-33.888584,151.1873473&key=AIzaSyATo66aR1XW_0vPRGB6CjsrCBDjaYi9ZUM"
 
 
-//https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=sushi&inputtype=textquery&key=AIzaSyATo66aR1XW_0vPRGB6CjsrCBDjaYi9ZUM
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + kw + "&inputtype=textquery&place_id&fields=photos,formatted_address,geometry,icon,name,permanently_closed,price_level,user_ratings_total,opening_hours,rating,place_id&locationbias=circle:2000@-33.888584,151.1873473&key=AIzaSyATo66aR1XW_0vPRGB6CjsrCBDjaYi9ZUM"
 
-// "&inputtype=textquery&fields=opening_hours&icon&geometry&formatted_address&geometry&icon&name&permanently_closed&photos&place_id&plus_code&types&ipbias";
-
-    
     $.ajax({
         url: queryURL,
         method: 'get',
-        // headers: {
-        //     'Access-Control-Allow-Origin': '*'
-        // },
         cache: false,
     }).then(function (list) {
 
-        console.log(list)
+        var placeID = list.candidates[0].place_id;
 
+        var detailList = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=" + placeID + "&fields=name,rating,formatted_phone_number,formatted_address,geometry,plus_code,opening_hours,photos,price_level,rating,reviews&key=AIzaSyATo66aR1XW_0vPRGB6CjsrCBDjaYi9ZUM";
+
+        $.ajax({
+            url: detailList,
+            method: 'get',
+            cache: false,
+        }).then(function (detailListResult) {
+
+            console.log(detailListResult);
+
+            var firstImageReference = detailListResult.result.photos[0].photo_reference;
+
+            var firstImageURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + firstImageReference + "&key=AIzaSyATo66aR1XW_0vPRGB6CjsrCBDjaYi9ZUM"
+
+            $.ajax({
+                url: firstImageURL,
+                method: 'get',
+                cache: false,
+            }).then(function (firstImageResult) {
+
+                console.log(firstImageResult);
+
+
+            });
+        });
     });
 
 });
